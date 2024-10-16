fdb142cb5c47de58d2712544294fbf7ef48b2e8a
sun <1695730274@qq.com>
2024-10-16 18:27:54 +0800

    ex2

--- Ex2/ex2a.py
+++ Ex2/ex2a.py
@@ -4,36 +4,46 @@ from bitcoin.core.script import *
 from utils import *
 from config import my_private_key, my_public_key, my_address, faucet_address
 from ex1 import send_from_P2PKH_transaction
+from bitcoin.wallet import CBitcoinAddress, CBitcoinSecret, P2PKHBitcoinAddress
 
 
 cust1_private_key = CBitcoinSecret(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    'cTKkodPzkCsr8ZArAarBJynova92PxYXr1penRP4LtdFRuDyRt3h')
 cust1_public_key = cust1_private_key.pub
 cust2_private_key = CBitcoinSecret(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    'cRpXhfvFxvhdWRjpa8C1ZAv6XsydxsS2FtydF5EJE4EgS8xuxxqV')
 cust2_public_key = cust2_private_key.pub
 cust3_private_key = CBitcoinSecret(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    'cNrRqR1HmnDecK8U1qneToR2fu1kVb1uHa7W1vsa3yktGEqrRQrS')
 cust3_public_key = cust3_private_key.pub
 
 
 ######################################################################
 # TODO: Complete the scriptPubKey implementation for Exercise 2
-
+my_private_key = CBitcoinSecret('cUaHgy7dvDm3b3shFPU6vBXd6Jv6q176FHBP1u2sXWA1dePpQ9p3')
+my_public_key = my_private_key.pub
 # You can assume the role of the bank for the purposes of this problem
 # and use my_public_key and my_private_key in lieu of bank_public_key and
 # bank_private_key.
 
-ex2a_txout_scriptPubKey = []
+ex2a_txout_scriptPubKey = CScript([
+    2,  # OP_2
+    my_public_key,
+    cust1_public_key,
+    cust2_public_key,
+    cust3_public_key,
+    4,  # OP_4
+    OP_CHECKMULTISIG
+])
 ######################################################################
 
 if __name__ == '__main__':
     ######################################################################
     # TODO: set these parameters correctly
-    amount_to_send = None
+    amount_to_send = 0.0001
     txid_to_spend = (
-        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-    utxo_index = None
+        '9ad7b3bafb860ccfaa5348f09d22e5e9ceabdef4e601852f8b6b81f35eedf3f3')
+    utxo_index = 1
     ######################################################################
 
     response = send_from_P2PKH_transaction(
--- Ex2/ex2b.py
+++ Ex2/ex2b.py
@@ -6,6 +6,7 @@ from config import my_private_key, my_public_key, my_address, faucet_address
 from ex1 import P2PKH_scriptPubKey
 from ex2a import (ex2a_txout_scriptPubKey, cust1_private_key, cust2_private_key,
                   cust3_private_key)
+from bitcoin.wallet import CBitcoinAddress, CBitcoinSecret, P2PKHBitcoinAddress
 
 
 def multisig_scriptSig(txin, txout, txin_scriptPubKey):
@@ -20,8 +21,8 @@ def multisig_scriptSig(txin, txout, txin_scriptPubKey):
     ######################################################################
     # TODO: Complete this script to unlock the BTC that was locked in the
     # multisig transaction created in Exercise 2a.
-    return []
-    ######################################################################
+    return [OP_0,bank_sig ,cust1_sig]   
+     ######################################################################
 
 
 def send_from_multisig_transaction(amount_to_send, txid_to_spend, utxo_index,
@@ -39,9 +40,9 @@ def send_from_multisig_transaction(amount_to_send, txid_to_spend, utxo_index,
 if __name__ == '__main__':
     ######################################################################
     # TODO: set these parameters correctly
-    amount_to_send = None
-    txid_to_spend = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-    utxo_index = None
+    amount_to_send = 0.00005
+    txid_to_spend = '12659e8fda118c0e04f1887f9dcc49509f73dcaecc0988235ff6b8855392823c'
+    utxo_index = 0
     ######################################################################
 
     txin_scriptPubKey = ex2a_txout_scriptPubKey
